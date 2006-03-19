9720b5ae8bd2692f310318b3be38d4a1a16ab9eb
Michael Vogt <michael.vogt@ubuntu.com>
2006-03-19 23:06:49 +0100

    * fontconfig/ko_KR: new version from the ubuntu-ko team (thanks!) * fontconfig/jp_JP: disable embedded bitmaps for japanese, thanks   to Jun Kobayashi (ubuntu: #35567) * fontconfig/zh_SG: added new configuration (based on zh_CN) * fontconfig-voodoo: add more missing options   (--list, --set, --quiet, --auto, --force)

--- LanguageSelector/FontConfig.py
+++ LanguageSelector/FontConfig.py
@@ -15,11 +15,11 @@ import os.path
 
 from LocaleInfo import LocaleInfo
 
-class FontConfigHackException(Exception):
+class ExceptionNotSymlink(Exception):
     pass
-class FontConfigHackExceptionNotSymlink(Exception):
+class ExceptionUnconfigured(Exception):
     pass
-class FontConfigHackExceptionUnconfigured(Exception):
+class ExceptionNoConfigForLocale(Exception):
     pass
 
 class FontConfigHack(object):
@@ -45,41 +45,44 @@ class FontConfigHack(object):
         """ returns the current language configuration as a string (e.g. zh_CN)
         
             if the configfile is not a symlink it raises a
-             FontConfigHackExceptionNotSymlink exception
+             ExceptionNotSymlink exception
             if the file dosn't exists raise a
-             FontConfigHackExceptionUnconfigured exception
+             ExceptionUnconfigured exception
             if it's unconfigured return the string 'none'
         """
         f = self.configFile
         if not os.path.exists(f):
-            raise FontConfigHackExceptionUnconfigured()
+            raise ExceptionUnconfigured()
         if not os.path.islink(f):
-            raise FontConfigHackExceptionNotSymlink()
+            raise ExceptionNotSymlink()
         realpath = os.path.realpath(f)
         return os.path.basename(realpath)
-    def setConfigBasedOnLocale(self):
-        """ set the configuration based on the locale in LocaleInfo.
-            return True if something was changed
-            return False if nothing needed to be done
-            can throw a exception
-        """
-        res = True
-        lang = self.li.getDefaultLanguage()
-        # do sanity checking
-        if os.path.exists(self.configFile) and not os.path.islink(self.configFile):
-            raise FontConfigHackExceptionNotSymlink()
 
-        # remove existing stuff
+    def setConfig(self, locale):
+        """ set the configuration for 'locale'. if locale can't be
+            found a NoConfigurationForLocale exception it thrown
+        """
+        # check if we have a config
+        if locale not in self.getAvailableConfigs():
+            raise ExceptionNoConfigForLocale()
+        # do sanity checking (is it really a symlink?)
+        if os.path.exists(self.configFile) and not os.path.islink(self.configFile):
+            raise ExceptionNotSymlink()
+        # remove existing symlink
         if os.path.exists(self.configFile):
             os.unlink(self.configFile)
-
-        # if we don't have a configuration make it "none"
-        if not lang in self.getAvailableConfigs():
-            lang = "none"
-            res = False
-        os.symlink("%s/%s"% (self.datadir, lang),
+        # do the actual symlink
+        os.symlink(os.path.normpath("%s/%s"% (self.datadir, locale)),
                    self.configFile)
-        return res
+        
+    def setConfigBasedOnLocale(self):
+        """ set the configuration based on the locale in LocaleInfo. If
+            no configuration is found the fontconfig config is set to
+            'none'
+            Can throw a exception
+        """
+        lang = self.li.getDefaultLanguage()
+        self.setConfig(lang)
         
 
 if __name__ == "__main__":
@@ -97,9 +100,9 @@ if __name__ == "__main__":
     print fc.getAvailableConfigs()
     try:
         config = fc.getCurrentConfig()
-    except FontConfigHackExceptionNotSymlink:
+    except ExceptionNotSymlink:
         print "not symlink"
-    except FontConfigHackExceptionUnconfigured:
+    except ExceptionUnconfigured:
         print "unconfigured"
 
     print fc.setConfigBasedOnLocale()
